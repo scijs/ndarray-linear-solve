49cbc2d284e08f4a93766801b7f56b4cd937141a
James Halliday <mail@substack.net>
2014-05-27 19:16:57 -0700

    free the scratch arrays

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var solve = require('ndarray-lu-solve');
 var crout = require('ndarray-crout-decomposition');
 var ndarray = require('ndarray');
-var zeros = require('ndarray-scratch').zeros;
+var scratch = require('ndarray-scratch');
 
 module.exports = function (A, B) {
     if (!A.dimension) {
@@ -16,12 +16,15 @@ module.exports = function (A, B) {
     if (A.dimension !== 2) throw new Error('not a 2-dimensional matrix');
     if (m !== n) throw new Error('not a square matrix: ' + m + 'x' + n);
     
-    var L = zeros([ m, m ]);
-    var U = zeros([ m, m ]);
+    var L = scratch.zeros([ m, m ]);
+    var U = scratch.zeros([ m, m ]);
     var ok = crout(A, L, U);
     if (!ok) return undefined;
     
     var X = ndarray(new Float64Array(m));
     var Y = ndarray(new Float64Array(m));
-    return solve(L, U, B, X, Y);
+    var res = solve(L, U, B, X, Y);
+    scratch.free(L);
+    scratch.free(U);
+    return res;
 };
